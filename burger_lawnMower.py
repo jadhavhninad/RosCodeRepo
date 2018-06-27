9521b69b7c81e6f963730153232ec142f3ad10cb
Ninad Jadhav (Student) <nhjadhav@en4146272l.fulton.ad.asu.edu>
2018-06-26 20:53:11 -0700

    updated movement

--- burger_lawnMower.py
+++ burger_lawnMower.py
@@ -4,41 +4,97 @@ lawn mower motion for turtlebot
 '''
 
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Point, Quaternion
 from tf2_msgs.msg import TFMessage
+import tf
 import time
 import numpy as np
 import re
+from math import radians, copysign, sqrt, pow, pi, atan2
+from tf.transformations import euler_from_quaternion
 
 
 
-def pos_callback(msg):
-    #rospy.loginfo(rospy.get_caller_id())
-    #rospy.loginfo(type(msg))
-    data = str(msg.transforms[0])
-    #print(data)
 
-    #vals = re.findall('\d+\.\d', data)
-    vals = [float(s) for s in re.findall(r'-?\d+\.?\d*', data)]
-    #print(vals)
+class Turtlebot_Mover:
+    def __init__(self):
+        self.x_pos=0
+        self.y_pos=0
+        self.cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=5)
+        self.position = Point()
+        self.move_cmd = Twist()
+        self.tf_listener = tf.TransformListener()
+        self.odom_frame = 'odom'
 
-    x_pos = vals[3]
-    y_pos = vals[4]
+        try:
+            self.tf_listener.waitForTransform(self.odom_frame, 'base_footprint', rospy.Time(), rospy.Duration(1.0))
+            self.base_frame = 'base_footprint'
+        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+            try:
+                self.tf_listener.waitForTransform(self.odom_frame, 'base_link', rospy.Time(), rospy.Duration(1.0))
+                self.base_frame = 'base_link'
+            except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+                rospy.loginfo("Cannot find transform between odom and base_link or base_footprint")
+                rospy.signal_shutdown("tf Exception")
 
-    print("initial pos = [", x_pos, "," , y_pos, "]")
-    #signal shutdown
-    rospy.signal_shutdown("Initial Pos extracted")
+    def pos_callback(self,msg):
+        #rospy.loginfo(rospy.get_caller_id())
+        #rospy.loginfo(type(msg))
+        data = str(msg.transforms[0])
+        print(data)
+
+        #vals = re.findall('\d+\.\d', data)
+        vals = [float(s) for s in re.findall(r'-?\d+\.?\d*', data)]
+        print(vals)
+
+        self.x_pos = vals[3]
+        self.y_pos = vals[4]
+
+        print("initial pos = [", self.x_pos, "," , self.y_pos, "]")
+        #signal shutdown
+        rospy.signal_shutdown("Initial Pos extracted")
 
 
-def getPos():
-    #while not rospy.is_shutdown():
-    rospy.Subscriber("tf", TFMessage, pos_callback)
+    def getPos(self):
+        #while not rospy.is_shutdown():
+        rospy.Subscriber("tf", TFMessage, self.pos_callback)
+
+    def move_x(self):
+        distance = 1.8
+        (position, rotation) = self.get_odom()
+        goal_x = position.x + distance
+        goal_y = position.y + distance
+
+        while distance > 0.02:
+            (position, rotation) = self.get_odom()
+            distance = sqrt(pow((goal_x - position.x), 2) + pow((goal_y - position.y), 2))
+            self.move_cmd.linear.x = min(1 * distance, 0.1)
+            self.cmd_vel.publish(self.move_cmd)
+
+
+        print("Moved to the end of grid. Now to rotate")
+        #rospy.signal_shutdown("Initial Pos extracted")
+
+
+    def get_odom(self):
+        try:
+            (trans, rot) = self.tf_listener.lookupTransform(self.odom_frame, self.base_frame, rospy.Time(0))
+            rotation = euler_from_quaternion(rot)
+
+        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+            rospy.loginfo("TF Exception")
+            return
+
+        return (Point(*trans), rotation[2])
+
 
 if __name__ == '__main__':
     rospy.init_node('turtlebot3_obstacle')
+    tb_obj = Turtlebot_Mover()
     try:
-        getPos()
+        #tb_obj.getPos()
         #The program keeps running till node does not shutdown.
+        tb_obj.move_x()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
