b6f9b57b25a2194e2bb6d42599214865c426a771
Ninad Jadhav (Student) <nhjadhav@en4146272l.fulton.ad.asu.edu>
2018-07-03 20:53:19 -0700

    calling gotoPoint

--- burger_lawnMower_old.py
+++ burger_lawnMower_old.py
@@ -4,7 +4,6 @@ lawn mower motion for turtlebot
 '''
 
 import rospy
-from gotopoint import GotoPoint
 from geometry_msgs.msg import Twist, Point, Quaternion
 from tf2_msgs.msg import TFMessage
 import tf
@@ -16,106 +15,18 @@ from tf.transformations import euler_from_quaternion
 
 
 
-class TurtlebotMover():
 
-    def __init__(self, goal_x, goal_y, gird_points):
-        self.goal_X = goal_x
-        self.goal_Y = goal_y
-        self.grid_points = gird_points
-        self.odom_frame = 'odom'
-        self.tf_listener = tf.TransformListener()
-        try:
-            self.tf_listener.waitForTransform(self.odom_frame, 'base_footprint', rospy.Time(), rospy.Duration(1.0))
-            self.base_frame = 'base_footprint'
-        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
-            try:
-                self.tf_listener.waitForTransform(self.odom_frame, 'base_link', rospy.Time(), rospy.Duration(1.0))
-                self.base_frame = 'base_link'
-            except (tf.Exception, tf.ConnectivityException, tf.LookupException):
-                rospy.loginfo("Cannot find transform between odom and base_link or base_footprint")
-                rospy.signal_shutdown("tf Exception")
-
-    def MoveBot(self):
-
-        (position, rotation) = self.get_odom()
-        init_y = position.y
-
-        while(init_y < self.goal_Y):
-
-            #Logic for generating sub goals and calling GotoPoint
-            (position, rotation) = self.get_odom()
-            GotoPoint(self.goal_X, position.y, 0)
-
-            (position, rotation) = self.get_odom()
-            GotoPoint(position.x, self.goal_Y/self.grid_points, 0)
-            init_y = init_y + self.goal_Y/self.grid_points
-
-            (position, rotation) = self.get_odom()
-            GotoPoint(-self.goal_X, position.y, 0)
-
-            (position, rotation) = self.get_odom()
-            GotoPoint(position.x, self.goal_Y / self.grid_points, 0)
-            init_y = init_y + self.goal_Y / self.grid_points
-
-
-    def get_odom(self):
-        try:
-            (trans, rot) = self.tf_listener.lookupTransform(self.odom_frame, self.base_frame, rospy.Time(0))
-            rotation = euler_from_quaternion(rot)
-
-        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
-            rospy.loginfo("TF Exception")
-            return
-
-        return (Point(*trans), rotation[2])
-
-
-if __name__ == '__main__':
-    rospy.init_node('turtlebot3_obstacle')
-    tb_obj = TurtlebotMover(1.2,1.2,4)
-    try:
-        #tb_obj.getPos()
-        #The program keeps running till node does not shutdown.
-        tb_obj.MoveBot()
-        #rospy.spin()
-    except rospy.ROSInterruptException:
-        pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
 class Turtlebot_Mover:
 
-    def __init__(self, gird_points):
+    def __init__(self):
         self.x_pos=0
         self.y_pos=0
-        self.distance_x = 0.2 * gird_points
-        self.distance_y = 0.2 * gird_points
         self.cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=5)
         self.position = Point()
         self.move_cmd = Twist()
         self.tf_listener = tf.TransformListener()
         self.odom_frame = 'odom'
         self.r=rospy.Rate(10)
-        self.last_rotation = 0
-        self.linear_speed = 1
-        self.angular_speed = 1
 
         try:
             self.tf_listener.waitForTransform(self.odom_frame, 'base_footprint', rospy.Time(), rospy.Duration(1.0))
@@ -128,25 +39,54 @@ class Turtlebot_Mover:
                 rospy.loginfo("Cannot find transform between odom and base_link or base_footprint")
                 rospy.signal_shutdown("tf Exception")
 
+
+
+    def pos_callback(self,msg):
+        #rospy.loginfo(rospy.get_caller_id())
+        #rospy.loginfo(type(msg))
+        data = str(msg.transforms[0])
+        print(data)
+
+        #vals = re.findall('\d+\.\d', data)
+        vals = [float(s) for s in re.findall(r'-?\d+\.?\d*', data)]
+        print(vals)
+
+        self.x_pos = vals[3]
+        self.y_pos = vals[4]
+
+        print("initial pos = [", self.x_pos, "," , self.y_pos, "]")
+        #signal shutdown
+        rospy.signal_shutdown("Initial Pos extracted")
+
+
+    def getPos(self):
+        #while not rospy.is_shutdown():
+        rospy.Subscriber("tf", TFMessage, self.pos_callback)
+
+
+    def move_x(self):
+
+
         #Reset Velocities
         self.move_cmd.linear.x = 0.0
         self.move_cmd.angular.z = 0.0
         self.cmd_vel.publish(self.move_cmd)
 
-    def move_x(self):
         #Moving along the X-Axis direction
-        distance_Y = 0.2
+        distance_X = 1.2
+        distance_Y = 0.4
         turn=1
         angle=180
-
         (position, rotation) = self.get_odom()
-        current_y_pos = position.y
+        original_y_pos = position.y
+        final_y = 0
         iteration=0
 
-        while (current_y_pos < self.goal_y):
+        while (original_y_pos + distance_X > final_y):
+
+            distance = distance_X
 
             #Moving along the X-axis
-            distance = self.distance_x
             (position, rotation) = self.get_odom()
             goal_x = position.x + distance*turn
             goal_y = position.y
@@ -272,6 +212,9 @@ class Turtlebot_Mover:
         #for Stopping the current motion.
         self.cmd_vel.publish(Twist())
 
+
+
+
         #rospy.signal_shutdown("Initial Pos extracted")
 
 
@@ -286,4 +229,14 @@ class Turtlebot_Mover:
 
         return (Point(*trans), rotation[2])
 
-"""
+
+if __name__ == '__main__':
+    rospy.init_node('turtlebot3_obstacle')
+    tb_obj = Turtlebot_Mover()
+    try:
+        #tb_obj.getPos()
+        #The program keeps running till node does not shutdown.
+        tb_obj.move_x()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
