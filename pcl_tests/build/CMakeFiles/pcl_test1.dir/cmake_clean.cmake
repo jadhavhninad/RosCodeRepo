07ceefaec8339754d10aff97feb0677b7c49afae
jadhavhninad <jadhavhninad@gmail.com>
2018-12-27 20:30:50 +0530

    initial commit

---
+++ pcl_tests/build/CMakeFiles/pcl_test1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl_test1.dir/pcl_test1.cpp.o"
+  "pcl_test1.pdb"
+  "pcl_test1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl_test1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
