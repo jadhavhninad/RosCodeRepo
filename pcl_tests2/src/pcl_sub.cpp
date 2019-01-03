b51e034ba82e4670a3ceb346683bae5d158a986a
jadhavhninad <jadhavhninad@gmail.com>
2019-01-03 23:08:20 +0530

    voxel filter publisher completed

--- pcl_tests2/src/pcl_sub.cpp
+++ pcl_tests2/src/pcl_sub.cpp
@@ -7,24 +7,30 @@
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 ros::Publisher op_pub;
 
+//=====This is the callback function which is used by subscriber====
 void callback(const PointCloud::ConstPtr& msg)
 {
   
-  pcl::PCLPointCloud2 cloud_filtered;
+  //pcl::PCLPointCloud2 cloud_filtered;
+  pcl::PointCloud<pcl::PointXYZ> msg_filtered;
 
-  // Perform the actual filtering
-  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+  //============Perform the actual filtering==============
+  //pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+  pcl::VoxelGrid<pcl::PointXYZ> sor; //filter object
   sor.setInputCloud (msg);
-  sor.setLeafSize (0.1, 0.1, 0.1);
-  sor.filter (cloud_filtered);
   
-  printf ("Original Cloud: width = %d, height = %d\n", msg->width, msg->height);
-  printf ("Filtered Cloud: width = %d, height = %d\n", cloud_filtered->width, cloud_filtered->height);
+  sor.setLeafSize (0.1, 0.1, 0.1); //default for now.Need to understand more of the leaf size.
+  sor.filter(msg_filtered);
+  
+  //printf ("Original Cloud: width = %d, height = %d\n", msg->width, msg->height);
+  //printf ("filtered cloud: width = %d, height = %d\n", msg_filtered->width, msg_filtered->height);
+  printf ("=============");
+  
   //BOOST_FOREACH (const pcl::PointXYZ& pt, msg->points)
     //printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
   
   //publish the filtered pointcloud to a new topic
-  op_pub.publish(cloud_filtered);
+  op_pub.publish(msg_filtered);
 }
 
 int main(int argc, char** argv)
